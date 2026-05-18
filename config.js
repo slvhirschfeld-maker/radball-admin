a79ebdc2f70d2ed8592e819b8c4397185400ad46
Gordon <hirschfeld@slv-halle.de>
2026-05-18 08:48:05 +0200

    v14

---
+++ config.js
@@ -0,0 +1,8 @@
+// ============================================================
+// config.js – Supabase Zugangsdaten
+// Diese Datei NICHT ins öffentliche Git-Repository!
+// In .gitignore eintragen: config.js
+// ============================================================
+
+const SB_URL = 'https://btpohaqsqajmwztmrrcp.supabase.co';  
+const SB_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0cG9oYXFzcWFqbXd6dG1ycmNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxNDY1NzEsImV4cCI6MjA5MzcyMjU3MX0.wQzp50utSWzOWZrOL4BM1s9dbiPk8duLajh9B7bBSOM'; 
