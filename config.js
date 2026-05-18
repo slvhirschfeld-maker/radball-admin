8a2528481cf64c737d9356b73080c15b2e839c1b
Gordon <hirschfeld@slv-halle.de>
2026-05-18 09:51:25 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
 // In .gitignore eintragen: config.js
 // ============================================================
 
-const SB_URL = 'https://btpohaqsqajmwztmrrcp.supabase.co';  
-const SB_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0cG9oYXFzcWFqbXd6dG1ycmNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxNDY1NzEsImV4cCI6MjA5MzcyMjU3MX0.wQzp50utSWzOWZrOL4BM1s9dbiPk8duLajh9B7bBSOM'; 
+SB_URL = 'https://btpohaqsqajmwztmrrcp.supabase.co';  
+SB_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0cG9oYXFzcWFqbXd6dG1ycmNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxNDY1NzEsImV4cCI6MjA5MzcyMjU3MX0.wQzp50utSWzOWZrOL4BM1s9dbiPk8duLajh9B7bBSOM'; 
